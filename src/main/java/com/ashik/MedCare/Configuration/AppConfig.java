4e1b07d54a1436dfa6a1b5a73c048fde8a19259f
Ashik <ashik23402@gmail.com>
2023-09-29 01:15:53 +0600

    cors update

--- src/main/java/com/ashik/MedCare/Configuration/AppConfig.java
+++ src/main/java/com/ashik/MedCare/Configuration/AppConfig.java
@@ -37,11 +37,11 @@ public class AppConfig {
                                                             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 
                                                                 CorsConfiguration cfg = new CorsConfiguration();
-//                                                                cfg.setAllowedOrigins(
-//                                                                        List.of("http://localhost:3000/")
-//                                                                );
+                                                                cfg.setAllowedOrigins(
+                                                                        List.of("http://localhost:3000/","*","https://medcare-testing.netlify.app/")
+                                                                );
 
-                                                                cfg.setAllowedOrigins(Collections.singletonList("*"));
+//                                                                cfg.setAllowedOrigins(Collections.singletonList("*"));
 
                                                                 cfg.setAllowedMethods(Collections.singletonList("*"));
                                                                 cfg.setAllowCredentials(true);
