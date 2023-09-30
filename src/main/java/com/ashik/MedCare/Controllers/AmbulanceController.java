1c4420f28a76cc05fdfab76f8e1120dadae779f2
Ashik <ashik23402@gmail.com>
2023-09-30 20:47:42 +0600

    asd

--- src/main/java/com/ashik/MedCare/Controllers/AmbulanceController.java
+++ src/main/java/com/ashik/MedCare/Controllers/AmbulanceController.java
@@ -20,7 +20,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
