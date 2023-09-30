8cc6c3cd181ad6f442787d82d5047c65c0f5d08c
Ashik <ashik23402@gmail.com>
2023-09-30 20:28:00 +0600

    cors

--- src/main/java/com/ashik/MedCare/Configuration/AppConfig.java
+++ src/main/java/com/ashik/MedCare/Configuration/AppConfig.java
@@ -38,7 +38,8 @@ public class AppConfig {
 
                                                                 CorsConfiguration cfg = new CorsConfiguration();
                                                                 cfg.setAllowedOrigins(
-                                                                        List.of("https://medcare-testing.netlify.app/","http://localhost:3000/","https://medcare-map-test.netlify.app/")
+                                                                        List.of("https://medcare-testing.netlify.app/","http://localhost:3000/","https://medcare-map-test.netlify.app/",
+																		"https://medcare.netlify.app/","*")
                                                                 );
 
 //                                                                cfg.setAllowedOrigins(Collections.singletonList("*"));
